dc72999c20f5c8aa79b14dafeca101864eec8d45
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 14:19:13 +0300

    NameSurname
    internship

---
+++ NameSurname/NameSurname/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Zeynep";
+            string Surname = "Ceyhan";
+
+            Console.WriteLine("Adım: " + Name + "\nSoyadım: " + Surname);
+            Console.ReadLine();
+        }
+
+    }
+
+}
